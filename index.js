d7762c743c8602e2c1aa7f6174d9d62f1bf414e5
Jakub <brodka.jakub@gmail.com>
2024-10-05 17:42:44 +0200

    code improvement

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import { listAllFiles } from "./drive/google-api.js";
 import { pdfOCR } from "./services/ocr/ocr.js";
-import { convertPdfToImages } from "./utils/convertPdfToImage.js";
 import { downloadFile } from "./utils/downloadFile.js";
 import { logger } from "./utils/logger.js";
 
@@ -12,23 +11,21 @@ async function main() {
   const files = await listAllFiles(FOLDER_ID);
   for (const file of files) {
     try {
-      console.log(` 🧾 Downloading PDF: ${file.name}`);
+      logger.info(` 🧾 Downloading PDF: ${file.name}`);
       const pdfFilePath = await downloadFile(
         file.id,
         inputPdfFolder,
         file.name
       );
-      console.log(`🧾 PDF downloaded to: ${pdfFilePath}`);
+      logger.info(`🧾 PDF downloaded to: ${pdfFilePath}`);
       const ocrData = await pdfOCR(pdfFilePath);
-      console.log(ocrData);
+      logger.info(ocrData);
 
 
     } catch (err) {
       logger.error(`Error processing:${file.name} ${err}`);
     }
   }
-
-  console.log(files);
 }
 
 main();
--- services/ocr/ocr.js
+++ services/ocr/ocr.js
@@ -1,6 +1,6 @@
 import vision from "@google-cloud/vision";
 import dotenv from "dotenv";
-import fs from "fs";
+import fs from "fs-extra";
 import path from "path";
 import { convertPdfToImages } from "../../utils/convertPdfToImage.js";
 import { deleteFile } from "../../utils/deleteFile.js";
@@ -9,87 +9,94 @@ import { logger } from "../../utils/logger.js";
 dotenv.config();
 
 const VISION_AUTH = {
-    credentials: {
-        client_email: process.env.GOOGLE_CLIENT_EMAIL,
-        private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'), // Handling the private key newline issue
-    },
+  credentials: {
+    client_email: process.env.GOOGLE_CLIENT_EMAIL,
+    private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"), // Handling the private key newline issue
+  },
+  fallback: true, // Force use of REST API instead of gRPC
 };
 
 export async function pdfOCR(pdfFilePath) {
-    const inputPdfFolder = "./input-pdf";
-    const imagesFolder = "./images";
-    const outputTextFolder = "./output-text";
+  const inputPdfFolder = "./input-pdf";
+  const imagesFolder = "./images";
+  const outputTextFolder = "./output-text";
+  const fileNameWithoutExt = path.basename(pdfFilePath, ".pdf");
 
-    [inputPdfFolder, imagesFolder, outputTextFolder].forEach((folder) => {
-        if (!fs.existsSync(folder)) {
-            fs.mkdirSync(folder, { recursive: true });
-        }
-    });
+  await Promise.all(
+    [inputPdfFolder, imagesFolder, outputTextFolder].map(fs.ensureDir)
+  );
 
-    try {
-        const imageFilePaths = await convertPdfToImages(pdfFilePath, imagesFolder);
-        logger.info(`🖼️ Converted PDF to images: ${imageFilePaths.join(", ")}`);
+  try {
+    const imageFilePaths = await convertPdfToImages(pdfFilePath, imagesFolder);
+    logger.info(`🖼️ Converted PDF to images: ${imageFilePaths.join(", ")}`);
 
-        if (imageFilePaths.length === 0) {
-            logger.error("No images were generated from the PDF");
-            return [];
-        }
-
-        const allResults = [];
-        for (const imageFilePath of imageFilePaths) {
-            const ocrResults = await fileOcr(imageFilePath, outputTextFolder);
-            allResults.push(...ocrResults); // Append each image's OCR result
-        }
-
-        // Concatenate all OCR results into a single string
-        const concatenatedResults = allResults
-            .map((result) => result.googleVisionText)
-            .join("\n");
-
-        // Save the concatenated OCR result to a single text file
-        const savePdfData = (folder, text) => {
-            const fileNameWithoutExt = path.basename(pdfFilePath, ".pdf");
-            const textPath = path.join(folder, `${fileNameWithoutExt}.txt`);
-            fs.writeFileSync(textPath, text, "utf8");
-        };
-
-        savePdfData(outputTextFolder, concatenatedResults);
-
-        logger.info(` 💚 Successfully processed and saved the OCR results for ${pdfFilePath}`);
-
-        // Delete the image files after processing
-        for (const imageFilePath of imageFilePaths) {
-            logger.warn(`Deleting temporary image: ${imageFilePath}`);
-            deleteFile(imageFilePath);
-        }
-
-        return concatenatedResults;
-    } catch (err) {
-        logger.error(`Error processing ${pdfFilePath}:`, err);
-        throw err;
+    if (imageFilePaths.length === 0) {
+      logger.error("No images were generated from the PDF");
+      return [];
     }
+
+    let concatenatedResults = "";
+    for (const imageFilePath of imageFilePaths) {
+      const ocrResult = await fileOcr(imageFilePath, outputTextFolder);
+      if (ocrResult) {
+        concatenatedResults += ocrResult.googleVisionText + "\n";
+      } else {
+        logger.warn(`No text found in image: ${imageFilePath}`);
+      }
+    }
+
+    await _saveDataToTxt(
+      outputTextFolder,
+      fileNameWithoutExt,
+      concatenatedResults
+    );
+
+    logger.info(
+      ` 💚 Successfully processed and saved the OCR results for ${pdfFilePath}`
+    );
+
+    // Delete the image files after processing
+    for (const imageFilePath of imageFilePaths) {
+      logger.warn(`Deleting temporary image: ${imageFilePath}`);
+      deleteFile(imageFilePath);
+    }
+
+    return concatenatedResults;
+  } catch (err) {
+    logger.error(`Error processing ${pdfFilePath}:`, err);
+    return "";
+  }
+}
+
+async function _saveDataToTxt(folder, fileNameWithoutExt, text) {
+  const textPath = path.join(folder, `${fileNameWithoutExt}.txt`);
+
+  try {
+    await fs.writeFile(textPath, text, "utf8");
+    logger.info(` 💚 Successfully saved the text file at: ${textPath}`);
+  } catch (err) {
+    logger.error(`Error saving the text file: ${err.message}`);
+  }
 }
 
 export async function fileOcr(imageFilePath) {
-    const client = new vision.ImageAnnotatorClient(VISION_AUTH);
-    const results = [];
+  const client = new vision.ImageAnnotatorClient(VISION_AUTH);
 
-    logger.info(` 🕶️ Processing image with Google Vision: ${imageFilePath}`);
-    try {
-        const [result] = await client.documentTextDetection(imageFilePath);
+  logger.info(` 🕶️ Processing image with Google Vision: ${imageFilePath}`);
+  try {
+    const [result] = await client.documentTextDetection(imageFilePath);
 
-        // Getting text from the image
-        let googleVisionText = "";
-        if (result.fullTextAnnotation) {
-            googleVisionText = result.fullTextAnnotation.text + "\n";
-            results.push({ googleVisionText });
-        }
-
-        logger.info(` 💚 Successfully processed image ${imageFilePath}`);
-    } catch (err) {
-        logger.error(`Error during Google Vision OCR processing: ${err.message}`);
-        throw err;
+    if (!result.fullTextAnnotation) {
+      return null;
     }
 
-    return results;
+    logger.info(` 💚 Successfully processed image ${imageFilePath}`);
+    const googleVisionText = result.fullTextAnnotation.text + "\n";
+    return { googleVisionText };
+  } catch (err) {
+    logger.error(`Error during Google Vision OCR processing: ${err.message}`);
+    // Instead of throwing an error, we'll just log it and continue
+  }
+
+  return null;
 }
