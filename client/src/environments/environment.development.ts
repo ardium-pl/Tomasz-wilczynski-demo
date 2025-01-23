aad382c696f256cd5360490a2c06ed7295d814dd
lolocob7 <lolocob7@gmail.com>
2025-01-23 13:02:42 +0100

    apiUrl fix

--- client/src/environments/environment.development.ts
+++ client/src/environments/environment.development.ts
@@ -2,5 +2,5 @@ import { Environment } from './types';
 
 export const environment: Environment = {
   production: false,
-  apiUrl: 'http://ocr-ekspert-podatkowy-dev.up.railway.app/api/',
+  apiUrl: 'https://ocr-ekspert-podatkowy-dev.up.railway.app/api/',
 };
